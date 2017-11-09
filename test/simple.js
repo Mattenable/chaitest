0c977e369f665d7ae131deae2d22d1f75281ef85
Matthew Nehrbass <mnehrbass@tenable.com>
2017-11-09 10:25:08 -0500

    initial

---
+++ test/simple.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('test', function() {
+    it('should pass', () => {
+        expect(true).to.be.true;
+    });
+});
